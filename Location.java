b7648831361ce6a779f91a29a78b8be93b76ec82
Pascal Baas <pascal.baas@outlook.com>
2016-03-31 08:58:54 +0200

    update2

--- Location.java
+++ Location.java
@@ -1,11 +1,6 @@
 
-/**
- * TODO Write a description of class Location here.
- *
- */
-public class Location
-{
-    // Floor, row and place positions
+public class Location {
+
     private int floor;
     private int row;
     private int place;
@@ -13,68 +8,61 @@ public class Location
     /**
      * Constructor for objects of class Location
      */
-    public Location(int floor, int row, int place)
-    {
+    public Location(int floor, int row, int place) {
         this.floor = floor;
         this.row = row;
         this.place = place;
     }
-    
+
     /**
-     * Implement content equality. // TODO check if this is needed
+     * Implement content equality.
      */
-    public boolean equals(Object obj)
-    {
+    public boolean equals(Object obj) {
         if(obj instanceof Location) {
             Location other = (Location) obj;
-            return floor == other.getFloor() && row == other.getRow() && place = other.getPlace();
+            return floor == other.getFloor() && row == other.getRow() && place == other.getPlace();
         }
         else {
             return false;
         }
     }
 
-     /**
+    /**
      * Return a string of the form floor,row,place.
      * @return A string representation of the location.
      */
-    public String toString()
-    {
+    public String toString() {
         return floor + "," + row + "," + place;
     }
-    
+
     /**
      * Use the 10 bits for each of the floor, row and place
      * values. Except for very big car parks, this should give
      * a unique hash code for each (floor, row, place) tupel.
      * @return A hashcode for the location.
      */
-    public int hashCode()
-    {
+    public int hashCode() {
         return (floor << 20) + (row << 10) + place;
     }
-    
+
     /**
      * @return The floor.
      */
-    public int getFloor()
-    {
+    public int getFloor() {
         return floor;
     }
-    
+
     /**
      * @return The row.
      */
-    public int getRow()
-    {
+    public int getRow() {
         return row;
     }
-    
+
     /**
      * @return The place.
      */
-    public int getPlace()
-    {
+    public int getPlace() {
         return place;
     }
 
