9524a84efc35ca5121764716aaac7722e6b06cf4
Xavier <xavier.kerkmeester@gmail.com>
2016-04-01 11:09:35 +0200

    Class Main added

---
+++ Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by xavier on 1-4-2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+       Simulator simulator = new Simulator();
+        simulator.run();
+    }
+}
+
