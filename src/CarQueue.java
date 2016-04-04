1a1fccdc56199b95b37f532f362e659dd67673fb
Pascal Baas <pascal.baas@outlook.com>
2016-04-04 14:23:02 +0200

    Update #12 - Added comments to CarQueue.java

--- src/CarQueue.java
+++ src/CarQueue.java
@@ -7,12 +7,24 @@ import java.util.Queue;
  */
 
 public class CarQueue {
+    /**
+     * Constructor for the class CarQueue that is initialized with objects of the class Car
+     */
     private Queue<Car> queue = new LinkedList<>();
 
+    /**
+     * Add a Car to the CarQueue.
+     * @param car Define the name of the car.
+     * @return Add car to queue.
+     */
     public boolean addCar(Car car) {
         return queue.add(car);
     }
 
+    /**
+     * Remove a Car from the CarQueue.
+     * @return Remove the first object in the list.
+     */
     public Car removeCar() {
         return queue.poll();
     }
