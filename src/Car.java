ac9de3cdcd68647456212ffeebad8c84c1e10f1c
Pascal Baas <pascal.baas@outlook.com>
2016-04-04 11:15:13 +0200

    Update #11 - Added comments to the Car class.  - Methods.

--- src/Car.java
+++ src/Car.java
@@ -15,30 +15,57 @@ public abstract class Car {
 
     }
 
+    /**
+     * Method to retrieve the current Location of a Car.
+     * @return  The current Location of the current Car, based on the Location class.
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Method to set a new Location for a Car.
+     * @param location Define a new Location for the current Car (based on an existing Location).
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Method to retrieve the parking time a parked Car still has remaining.
+     * @return  The amount of parking time the current Car still has.
+     */
     public int getMinutesLeft() {
         return minutesLeft;
     }
 
+    /**
+     * Method to change the parking time of a parked Car.
+     * @param minutesLeft Define the amount of minutes to set minutesLeft to.
+     */
     public void setMinutesLeft(int minutesLeft) {
         this.minutesLeft = minutesLeft;
     }
-    
+
+    /**
+     * Method to check if a ticket is being paid for.
+     * @return Return 'true' if being paid for, return 'false' if not being paid for.
+     */
     public boolean getIsPaying() {
         return isPaying;
     }
 
+    /**
+     * Method to change the value of the isPaying boolean.
+     * @param isPaying  Define the new value. True means 'Yes', False means 'No'.
+     */
     public void setIsPaying(boolean isPaying) {
         this.isPaying = isPaying;
     }
 
+    /**
+     * Method to decrease the time a parked Car has remaining.
+     */
     public void tick() {
         minutesLeft--;
     }
