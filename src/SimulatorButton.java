a3820a486c37feb63c231d3e834a32d2f948c954
Stefan Voogd <s.voogd@gmail.com>
2016-04-07 11:27:27 +0200

    Week 1 v2

---
+++ src/SimulatorButton.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by stefa on 7-4-2016.
+ */
+public class SimulatorButton extends JFrame {
+}
