6716f49c68a0621b2e78a8f12263e452a93c6efb
Pascal Baas <pascal.baas@outlook.com>
2016-04-07 11:36:51 +0200

    Update #15 - Added SimulatorRunner.java (test)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-
-
 /**
  * @author      327278, 331048, 335364 & 343991
  * @version     01-04-2016
@@ -8,6 +6,7 @@
 public class Main {
     public static void main(String[] args) {
        Simulator simulator = new Simulator();
+       // simulator.run();
     }
 
 
---
+++ src/Runner/SimulatorRunner.java
@@ -0,0 +1,16 @@
+// package Runner;
+
+// import Main.*;
+
+/**
+ * @author      327278, 331048, 335364 & 343991
+ * @version     01-04-2016
+ */
+
+public class SimulatorRunner {
+    public static void main(String[] args) {
+        // Simulator simulator =
+        new Simulator();
+        // simulator.run();
+    }
+}
